d62b1610915f57a38309d2043538f08a21c6fdce
Eric Jackson <eric@deepweave.com>
2020-01-21 13:34:28 -0500

    sqlcmd

--- sftp.sh
+++ sftp.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-#Pull XML from DB
-# sqlcmd -S $sql_host -d munprod -y 0 -i StaffingXML.sql -o Staffing.xml
-# sqlcmd -S $sql_host -d munprod -y 0 -i PersonXML.sql -o Person.xml
-
 # Get env vars
 set -a
 source .env
 set +a
+
+#Pull XML from DB
+sqlcmd -S $sql_host -d $sql_db -y 0 -i StaffingXML.sql -o Staffing.xml
+sqlcmd -S $sql_host -d $sql_db -y 0 -i PersonXML.sql -o Person.xml
 
 echo $ftp_url
 echo $ftp_pw
@@ -24,10 +24,12 @@ personfilename=Person$date.xml
 echo $personfilename
 mv Person.xml $personfilename 
 
+# echo 'cd /DEV/WIM_OUT/' > psftp.scr
 echo 'cd /DEV/import/ongoing.unprocessed/' > psftp.scr
-echo 'put' $staffingfilename >> psftp.scr
-echo 'put' $personfilename >> psftp.scr
-echo 'close' >> psftp.scr
+echo 'get Staffing-12.xml' >> psftp.scr
+# echo 'put' $staffingfilename >> psftp.scr
+# echo 'put' $personfilename >> psftp.scr
+# echo 'close' >> psftp.scr
 
 "C:\Program Files\PuTTY\psftp.exe" -P 22 -l $ftp_user -pw $ftp_pw $ftp_url -b ".\psftp.scr" >> sftp.log
 # echo sftp $ftp_user@$ftp_url -b psftp.scr >> ftpcmd
