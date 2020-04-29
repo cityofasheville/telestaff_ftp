cbade77a72674ea527229c8fe369d96314e29e95
Eric Jackson <eric@deepweave.com>
2020-04-29 10:26:04 -0400

    delete old logs

---
+++ payroll_export/deleteOldLogs.ps1
@@ -0,0 +1,8 @@
+# Delete logfiles older than 30 day(s)
+$Path = "C:\telestaff_ftp\payroll_export\tmp"
+$Daysback = "-30"
+
+$CurrentDate = Get-Date
+$DatetoDelete = $CurrentDate.AddDays($Daysback)
+Get-ChildItem $Path | Where-Object { $_.LastWriteTime -lt $DatetoDelete } | Remove-Item #-WhatIf
+# Uncomment -WhatIf to see what files would be deleted
